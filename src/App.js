321d12558f1c4da0d610cac89d3d6c6eafa3ae55
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-06-09 16:04:01 -0300

    Colocando o react para começar a usar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>João Marques</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
