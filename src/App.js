dea8c6e62cfcf79a710359537630822f48978330
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-06-09 18:48:33 -0300

    Criando animação no titulo

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ class App extends Component {
           <div className="center-text-container">
             <span className="subhead-title">Olá! eu sou</span>
             <h1 className="name-title">João Marques</h1>
-            <h2 className="programming-title">Sou um programador Front-End</h2>
+            <h2 className="programming-title typewriter">Sou um programador Front-End</h2>
           </div>
         </main>
-        <h2>.</h2>
       </div>
     );
   }
