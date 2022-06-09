c0203bb3082ecce66891f042965ac1d986eb3c98
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-06-09 17:34:00 -0300

    Colocando Titulo e meu nome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import { Component } from 'react';
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>João Marques</h1>
+        <main className="main">
+          <div className="center-text-container">
+            <span className="subhead-title">Olá! eu sou</span>
+            <h1 className="name-title">João Marques</h1>
+            <h2 className="programming-title">Sou um programador Front-End</h2>
+          </div>
+        </main>
+        <h2>.</h2>
       </div>
     );
   }
