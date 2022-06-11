4d91642323512b72a17b29e94f4e3af6817d0e46
joãoMarques <j.v.marques.fernandes@gmail.com>
2022-06-11 05:57:55 -0300

    Melhoria em espaços e vigula no texto do projeto

--- src/PlaceHolderCard.jsx
+++ src/PlaceHolderCard.jsx
@@ -7,7 +7,7 @@ class PlaceHolderCard extends Component {
       <div className='project-card'>
         <img src={placeholder} alt="imagem Placeholder" className="project-img mg-auto" />
         <h3 className="center-text text-center">Sou apenas um placeholder</h3>
-        <p className="description-projects text-center mg-auto">Isso aqui vai ficar por um tempinho aqui enquanto estou desenvolvendo projetos bacanas :)</p>
+        <p className="description-projects text-center mg-auto">Isso aqui vai ficar por um tempinho aqui, enquanto estou desenvolvendo projetos bacanas :)</p>
         <hr className="project-line mg-auto"/>
         <h3 className="skills-project text-center">Javascript • React</h3>
       </div>
